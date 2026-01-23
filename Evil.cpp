b77588b2aba31865a0d78288c8d5b2b089bef21b
BIGSosiska228 <andreymolofia@gmail.com>
2026-01-23 23:40:11 +0400

    Update Evil.cpp
    Updated the constructor to include armor parameter.

--- Evil.cpp
+++ Evil.cpp
@@ -1,9 +1,10 @@
 #include "Evil.h"
 
-Evil::Evil(std::string n, int h, int d)
+Evil::Evil(std::string n, int h,int j, int d)
 {
     name = n;
     health = h;
+    armor = j;
     damage = d;
     lvl = 1;
 }
