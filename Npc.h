3f2efd976d3e2e1d7cc9955a9f13e6dfb2f7233c
BIGSosiska228 <andreymolofia@gmail.com>
2026-01-23 23:42:11 +0400

    Update Npc.h

--- Npc.h
+++ Npc.h
@@ -7,27 +7,53 @@ class Npc
 public:
     std::string name;
     int health;
+    int maxHealth;
     int damage;
+    int armor;
     int lvl;
+    int potions; 
 
     Npc()
     {
         name = "Npc";
         health = 10;
+        maxHealth = health;
+        armor = 5;
         damage = 2;
         lvl = 1;
+        potions = 2; 
     }
 
+    
     virtual void Create()
     {
+        maxHealth = health; 
+        potions = 2;
+    }
+
+
+    bool UsePotion()
+    {
+        if (potions > 0 && health < maxHealth)
+        {
+            health += 20;
+            if (health > maxHealth)
+                health = maxHealth;
+
+            potions--;
+            return true;
+        }
+        return false;
     }
 
     virtual void GetInfo()
     {
-        std::cout << "Èìÿ: " << name << std::endl;
-        std::cout << "Çäîðîâüå: " << health << std::endl;
-        std::cout << "Óðîí: " << damage << std::endl;
-        std::cout << "Óðîâåíü: " << lvl << std::endl;
+        std::cout << "Имя: " << name << std::endl;
+        std::cout << "Здоровье: " << health << "/" << maxHealth << std::endl;
+        std::cout << "Урон: " << damage << std::endl;
+        std::cout << "Броня: " << armor << std::endl;
+        std::cout << "Уровень: " << lvl << std::endl;
+        std::cout << "Зелья: " << potions << std::endl;
     }
 
     virtual std::string GetClassName()
@@ -37,4 +63,3 @@ public:
 
     virtual ~Npc() {}
 };
-
