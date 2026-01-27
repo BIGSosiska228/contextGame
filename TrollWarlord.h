b3e363aa5dd22ad9a6fdf73dd7e3957b9c5ceca0
BIGSosiska228 <andreymolofia@gmail.com>
2026-01-27 23:12:57 +0400

    Update TrollWarlord.h
    Refactor TrollWarlord class to encapsulate strength and weapons, add getter and setter methods.

--- TrollWarlord.h
+++ TrollWarlord.h
@@ -1,13 +1,33 @@
 #pragma once
 #include "Npc.h"
+#include <string>
 
 class TrollWarlord : public Npc
 {
-public:
+private:
     int strength;
     std::string weapons[4];
 
+public:
     TrollWarlord();
-    void Create();
-    void GetInfo();
+
+    
+    void Create() override;
+    void GetInfo() const override;
+
+    
+    int GetStrength() const { return strength; }
+    std::string GetWeapon(int index) const
+    {
+        if (index < 0 || index > 3) return "";
+        return weapons[index];
+    }
+
+   
+    void SetStrength(int s) { strength = s; }
+    void SetWeapon(int index, const std::string& weapon)
+    {
+        if (index < 0 || index > 3) return;
+        weapons[index] = weapon;
+    }
 };
