f32ee04cf56ae1dbfe730f7b04d4e509c2138867
BIGSosiska228 <andreymolofia@gmail.com>
2026-01-27 23:10:45 +0400

    Update Npc.h
    Added new methods for health management and damage handling.

--- Npc.h
+++ Npc.h
@@ -4,15 +4,16 @@
 
 class Npc
 {
-public:
+private:
     std::string name;
     int health;
     int maxHealth;
     int damage;
     int armor;
     int lvl;
-    int potions; 
+    int potions;
 
+public:
     Npc()
     {
         name = "Npc";
@@ -21,32 +22,17 @@ public:
         armor = 5;
         damage = 2;
         lvl = 1;
-        potions = 2; 
-    }
-
-    
-    virtual void Create()
-    {
-        maxHealth = health; 
         potions = 2;
     }
 
 
-    bool UsePotion()
+    virtual void Create()
     {
-        if (potions > 0 && health < maxHealth)
-        {
-            health += 20;
-            if (health > maxHealth)
-                health = maxHealth;
-
-            potions--;
-            return true;
-        }
-        return false;
+        maxHealth = health;
+        potions = 2;
     }
 
-    virtual void GetInfo()
+    virtual void GetInfo() const
     {
         std::cout << "Имя: " << name << std::endl;
         std::cout << "Здоровье: " << health << "/" << maxHealth << std::endl;
@@ -56,10 +42,55 @@ public:
         std::cout << "Зелья: " << potions << std::endl;
     }
 
-    virtual std::string GetClassName()
+    virtual std::string GetClassName() const
     {
         return name;
     }
 
     virtual ~Npc() {}
+
+    void SetHealth(int hp)
+    {
+        health = hp;
+        maxHealth = hp;
+    }
+
+    int GetHealth() const { return health; }
+    int GetMaxHealth() const { return maxHealth; }
+    int GetDamage() const { return damage; }
+    int GetArmor() const { return armor; }
+    int GetLevel() const { return lvl; }
+    int GetPotions() const { return potions; }
+    std::string GetName() const { return name; }
+
+
+    void TakeDamage(int dmg)
+    {
+        int effectiveDmg = dmg - armor;
+        if (effectiveDmg < 0) effectiveDmg = 0;
+        health -= effectiveDmg;
+        if (health < 0) health = 0;
+    }
+
+    void Heal(int amount)
+    {
+        health += amount;
+        if (health > maxHealth) health = maxHealth;
+    }
+
+    bool UsePotion()
+    {
+        if (potions > 0 && health < maxHealth)
+        {
+            Heal(20);
+            potions--;
+            return true;
+        }
+        return false;
+    }
+
+    void AddDamage(int amount) { damage += amount; }
+    void AddArmor(int amount) { armor += amount; }
+    void AddPotions(int amount) { potions += amount; }
+    void SetName(const std::string& n) { name = n; }
 };
