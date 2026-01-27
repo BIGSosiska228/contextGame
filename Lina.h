f6c4e223c617c80290a6b16dc24a28416c1e3003
BIGSosiska228 <andreymolofia@gmail.com>
2026-01-27 23:10:17 +0400

    Update Lina.h
    Refactor Lina class to add accessors and mutators for intelligence and spells.

--- Lina.h
+++ Lina.h
@@ -1,13 +1,33 @@
 #pragma once
 #include "Npc.h"
+#include <string>
 
 class Lina : public Npc
 {
-public:
+private:
     std::string spells[3];
     int intelligence;
 
+public:
     Lina();
-    void Create();
-    void GetInfo();
+
+
+    void Create() override;
+    void GetInfo() const override;
+
+
+    int GetIntelligence() const { return intelligence; }
+    std::string GetSpell(int index) const
+    {
+        if (index < 0 || index > 2) return "";
+        return spells[index];
+    }
+
+
+    void SetIntelligence(int intel) { intelligence = intel; }
+    void SetSpell(int index, const std::string& spell)
+    {
+        if (index < 0 || index > 2) return;
+        spells[index] = spell;
+    }
 };
