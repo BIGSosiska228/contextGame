e1bf7aeba418d002aa584041e2da75e08c00f753
BIGSosiska228 <andreymolofia@gmail.com>
2026-01-27 23:07:27 +0400

    Update AntiMage.cpp
    Refactored AntiMage constructor and methods to use setter functions for name, health, damage, and armor. Updated Create method to set health and use input for name.

--- AntiMage.cpp
+++ AntiMage.cpp
@@ -3,26 +3,33 @@
 
 AntiMage::AntiMage()
 {
-    name = "Анти маг";
-    health = 25;
-    damage = 8;
-    armor = 1;
-    lvl = 1;
+
+    SetName("Анти маг");
+    TakeDamage(0);
+    AddDamage(8); 
+    AddArmor(1);  
+
+
     agility = 26;
 
     abilities[0] = "Мана брейк";
     abilities[1] = "Мана войд";
 }
 
+
 void AntiMage::Create()
 {
     std::cout << "Введите имя Anti mage: ";
-    std::cin >> name;
-    Npc::Create();
-}
-void AntiMage::GetInfo()
-{
-    Npc::GetInfo();
-    std::cout << "Ловкость: " << agility << std::endl;
+    std::string inputName;
+    std::cin >> inputName;
+    SetName(inputName);  
+    SetHealth(22);
+    Npc::Create();       
 }
 
+
+void AntiMage::GetInfo() const
+{
+    Npc::GetInfo();  
+    std::cout << "Ловкость: " << agility << std::endl;
+}
